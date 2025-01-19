9638f6a7d06671b7a08f11018e1308390058c075
Leonardo 'Alemax' Ferreira <leoalemax@gmail.com>
2025-01-19 19:21:06 -0300

    feat: Disable sddm and anable gdm

--- build.sh
+++ build.sh
@@ -9,4 +9,9 @@ rm /etc/sddm.conf.d/virtualkbd.conf
 systemctl disable bazzite-autologin.service
 systemctl enable ublue-update.timer
 
+sudo tar -xzvf ~/Downloads/sddm-sober.tar.gz -C /usr/share/sddm/themes
+
+systemctl disable sddm
+systemctl enable gdm
+
 ostree container commit
