e37595e7cbd8f1bf31556a130a8f2bf3b165f67f
Leonardo 'Alemax' Ferreira <leoalemax@gmail.com>
2025-01-19 20:38:17 -0300

    feat: Disable sddm and anable gdm

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ rm /etc/sddm.conf.d/virtualkbd.conf
 systemctl disable bazzite-autologin.service
 systemctl enable ublue-update.timer
 
-tar -xzvf /tmp/theme/sugar-dark.tar -C /usr/share/sddm/themes
+tar -xzvf /tmp/theme/sugar-dark.tar.gz -C /usr/share/sddm/themes
 
 cp /temp/sddm.conf /etc/sddm.conf
 
