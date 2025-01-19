18772d8994cb58f3dbe78f760cea0d098b824e31
Leonardo 'Alemax' Ferreira <leoalemax@gmail.com>
2025-01-19 18:16:09 -0300

    fix: Add ostree container commit

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 set -ouex pipefail
-dnf install -y tmux 
-
-systemctl enable podman.socket
+dnf install -y tmux
 
 rm /etc/sddm.conf.d/steamos.conf
 rm /etc/sddm.conf.d/virtualkbd.conf
 
 systemctl disable bazzite-autologin.service
 systemctl enable ublue-update.timer
+
+ostree container commit
