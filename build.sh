1abc0a3fbb894c0f5b21ceafe66d474da5fe61eb
Leonardo 'Alemax' Ferreira <leoalemax@gmail.com>
2025-01-19 21:33:14 -0300

    feat: Disable sddm and anable gdm

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 
 set -ouex pipefail
 dnf install -y tmux
-dnf install -y qt5-*
+
+dnf install -y qt5-qtbase-devel
+dnf install -y qt5-qtquickcontrols2 
+dnf install -y qt5-qtquickcontrols
+dnf install -y qt5-qtgraphicaleffects
 
 rm /etc/sddm.conf.d/steamos.conf
 rm /etc/sddm.conf.d/virtualkbd.conf
