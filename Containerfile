d9472e5123ce4c4a5d724ed13184d1fa4f8cdd31
Leonardo 'Alemax' Ferreira <leoalemax@gmail.com>
2025-01-19 21:37:45 -0300

    feat: Disable sddm and anable gdm

--- Containerfile
+++ Containerfile
@@ -1,9 +1,6 @@
-FROM ghcr.io/ublue-os/bazzite-deck-gnome:stable
+FROM ghcr.io/ublue-os/bazzite-deck:stable
 
 COPY build.sh /tmp/build.sh
-COPY sddm.conf /tmp/sddm.conf
-
-COPY theme /tmp/theme
 
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
--- build.sh
+++ build.sh
@@ -14,8 +14,4 @@ rm /etc/sddm.conf.d/virtualkbd.conf
 systemctl disable bazzite-autologin.service
 systemctl enable ublue-update.timer
 
-cp -r /tmp/theme/plasma-chili /usr/share/sddm/themes/plasma-chili
-
-cp /tmp/sddm.conf /etc/sddm.conf
-
 ostree container commit
