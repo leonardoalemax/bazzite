cb013434fbb8412f273069305270c09c2b682d38
Leonardo 'Alemax' Ferreira <leualemax@gmail.com>
2025-01-20 13:31:25 -0300

    feat: add codium install

--- build.sh
+++ build.sh
@@ -10,7 +10,9 @@ dnf install -y qt5-qtgraphicaleffects
 
 dnf install -y syncthing
 
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee /etc/yum.repos.d/vscodium.repo
 rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+
 dnf install -y codium
 
 dnf install -y zsh
